621f781fa19bf8064a3c1fda16906fb6a1f2d2c8
Kevin Hwang <k3vinhwang@gmail.com>
2015-01-28 23:45:19 -0800

    Add splash screen VC

---
+++ Streak Club/KHSplashScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHSplashScreenViewController.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 1/28/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHSplashScreenViewController : UIViewController
+
+@end
---
+++ Streak Club/KHSplashScreenViewController.m
@@ -0,0 +1,37 @@
+//
+//  KHSplashScreenViewController.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 1/28/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHSplashScreenViewController.h"
+
+@interface KHSplashScreenViewController ()
+
+@end
+
+@implementation KHSplashScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
