174c2ef8564db748719c4a77bb763e3370756287
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-27 13:54:17 -0700

    Add CustomFonts category

---
+++ Streak Club/UIFont+CustomFonts.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+CustomFonts.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/27/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (CustomFonts)
+
+@end
---
+++ Streak Club/UIFont+CustomFonts.m
@@ -0,0 +1,13 @@
+//
+//  UIFont+CustomFonts.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/27/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "UIFont+CustomFonts.h"
+
+@implementation UIFont (CustomFonts)
+
+@end
