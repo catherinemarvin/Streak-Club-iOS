f889981439bc7b688a8f5e9b66f91b0809100a4c
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-27 13:13:06 -0700

    Add colorWithHex

---
+++ Streak Club/UIColor+HexString.h
@@ -0,0 +1,14 @@
+//
+//  UIColor+HexString.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/27/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (HexString)
+
+
+@end
