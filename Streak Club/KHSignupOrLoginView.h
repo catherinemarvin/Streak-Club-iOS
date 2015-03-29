3c2ad4407d640cd6e1dd7e6a67abe0166f153852
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-28 21:04:47 -0700

    Add a view to the signup/login VC

---
+++ Streak Club/KHSignupOrLoginView.h
@@ -0,0 +1,13 @@
+//
+//  KHSignupOrLoginView.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/28/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHSignupOrLoginView : UIView
+
+@end
---
+++ Streak Club/KHSignupOrLoginView.m
@@ -0,0 +1,21 @@
+//
+//  KHSignupOrLoginView.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/28/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHSignupOrLoginView.h"
+
+@implementation KHSignupOrLoginView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
