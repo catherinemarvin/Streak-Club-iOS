6f4b6ed14ca8d09d4250323864000b9b22626df0
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-28 18:14:52 -0700

    Add KHSplashScreenContentView

---
+++ Streak Club/KHSplashScreenContentView.h
@@ -0,0 +1,13 @@
+//
+//  KHSplashScreenContentView.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/27/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHSplashScreenContentView : UIView
+
+@end
---
+++ Streak Club/KHSplashScreenContentView.m
@@ -0,0 +1,21 @@
+//
+//  KHSplashScreenContentView.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/27/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHSplashScreenContentView.h"
+
+@implementation KHSplashScreenContentView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
