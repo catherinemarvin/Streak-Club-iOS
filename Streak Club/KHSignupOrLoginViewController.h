dc736d9e01236a9dc7b9ed502ee03928bc95021d
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-28 20:59:42 -0700

    Add a signup or login VC

---
+++ Streak Club/KHSignupOrLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHSignupOrLoginViewController.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/28/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHSignupOrLoginViewController : UIViewController
+
+@end
