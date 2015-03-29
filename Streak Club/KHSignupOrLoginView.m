7fc1590ac4333b1c7662b1d1fd571c8af2b1d2cb
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-28 21:15:10 -0700

    Add requisite fields and buttons for the signup/login view

--- Streak Club/KHSignupOrLoginView.m
+++ Streak Club/KHSignupOrLoginView.m
@@ -13,12 +13,65 @@
 #import "UIFont+CustomFonts.h"
 #import "UIColor+HexString.h"
 
+@interface KHSignupOrLoginView()
+
+@property (nonatomic, strong) UITextField *usernameField;
+@property (nonatomic, strong) UITextField *passwordField;
+
+@property (nonatomic, strong) UIView *registerOnlyFields;
+@property (nonatomic, strong) UITextField *repeatPasswordField;
+@property (nonatomic, strong) UITextField *emailField;
+
+@property (nonatomic, strong) UIButton *loginButton;
+@property (nonatomic, strong) UIButton *registerButton;
+
+@end
+
 @implementation KHSignupOrLoginView
 
 - (instancetype)init {
     if (self = [super init]) {
         self.backgroundColor = [UIColor colorWithHexString:@"34a0f2"];
         
+        _usernameField = ({
+            UITextField *field = [[UITextField alloc] init];
+            
+            field;
+        });
+        [self addSubview:_usernameField];
+        
+        _passwordField = ({
+            UITextField *field = [[UITextField alloc] init];
+            field;
+        });
+        [self addSubview:_passwordField];
+        
+        _registerOnlyFields = [[UIView alloc] init];
+        [self addSubview:_registerOnlyFields];
+        
+        _repeatPasswordField = ({
+            UITextField *field = [[UITextField alloc] init];
+            field;
+        });
+        [_registerOnlyFields addSubview:_repeatPasswordField];
+        
+        _emailField = ({
+            UITextField *field = [[UITextField alloc] init];
+            field;
+        });
+        [_registerOnlyFields addSubview:_emailField];
+        
+        _loginButton = ({
+            UIButton *button = [[UIButton alloc] init];
+            button;
+        });
+        [self addSubview:_loginButton];
+        
+        _registerButton = ({
+            UIButton *button = [[UIButton alloc] init];
+            button;
+        });
+        [self addSubview:_registerButton];
     }
     return self;
 }
