79336564b58f0c3985938ae32dcbed3d9df1d052
Kevin Hwang <k3vinhwang@gmail.com>
2015-03-29 19:31:11 -0700

    Add KHSessionManager to manage the API key

---
+++ Streak Club/KHSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  KHSessionManager.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/29/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHSessionManager : NSObject
+
+@end
---
+++ Streak Club/KHSessionManager.m
@@ -0,0 +1,13 @@
+//
+//  KHSessionManager.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 3/29/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHSessionManager.h"
+
+@implementation KHSessionManager
+
+@end
