c45371a6aa3f51ea36df1ff76534e7deaafe674b
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-05 00:21:24 -0700

    Add global tab bar controller

---
+++ Streak Club/KHGlobalTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHGlobalTabBarViewController.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 4/5/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHGlobalTabBarViewController : UITabBarController
+
+@end
