0e1f43d67f2942173c25496ee28cf86759e4ff50
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-06 19:32:03 -0700

    Add home VC

---
+++ Streak Club/KHHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHHomeViewController.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 4/6/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHHomeViewController : UIViewController
+
+@end
