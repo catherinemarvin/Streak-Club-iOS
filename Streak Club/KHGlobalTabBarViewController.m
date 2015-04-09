bb6443a530d1c321d8a198675ea76fc2373f4604
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-08 20:55:03 -0700

    Add home view to the global tab

--- Streak Club/KHGlobalTabBarViewController.m
+++ Streak Club/KHGlobalTabBarViewController.m
@@ -8,30 +8,31 @@
 
 #import "KHGlobalTabBarViewController.h"
 
+// Tab Bar Sections
+#import "KHHomeViewController.h"
+
+typedef NS_ENUM(NSInteger, KHGlobalTabBarSection) {
+    KHGlobalTabBarSectionHome
+};
+
 @interface KHGlobalTabBarViewController ()
+
+@property (nonatomic, strong) KHHomeViewController *homeViewController;
 
 @end
 
 @implementation KHGlobalTabBarViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (instancetype)init {
+    if (self = [super init]) {
+        _homeViewController = [[KHHomeViewController alloc] init];
+        _homeViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Home", nil) image:[UIImage imageNamed:@"homeTabIcon"] tag:KHGlobalTabBarSectionHome];
+        UINavigationController *homeNav = [[UINavigationController alloc] initWithRootViewController:_homeViewController];
+        
+        NSArray *viewControllers = @[homeNav];
+        [self setViewControllers:viewControllers];
+    }
+    return self;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
