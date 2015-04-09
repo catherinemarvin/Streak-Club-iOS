830b7100c8a1fb00717cc6ba73f5951f959b5878
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-08 21:46:10 -0700

    Fix some capitalization issues

--- Streak Club/KHSignupOrLoginViewModel.h
+++ Streak Club/KHSignupOrLoginViewModel.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "KHSignupOrLoginViewProtocol.h"
 
-@interface KHSignuporLoginViewModel : NSObject
+@interface KHSignupOrLoginViewModel : NSObject
 
 - (instancetype)initWithView:(id<KHSignupOrLoginViewProtocol>)view;
 
--- Streak Club/KHSignupOrLoginViewModel.m
+++ Streak Club/KHSignupOrLoginViewModel.m
@@ -6,15 +6,15 @@
 //  Copyright (c) 2015 Kevin Hwang. All rights reserved.
 //
 
-#import "KHSignuporLoginViewModel.h"
+#import "KHSignupOrLoginViewModel.h"
 
-@interface KHSignuporLoginViewModel()
+@interface KHSignupOrLoginViewModel()
 
 @property (nonatomic, weak) id<KHSignupOrLoginViewProtocol>view;
 
 @end
 
-@implementation KHSignuporLoginViewModel
+@implementation KHSignupOrLoginViewModel
 
 - (instancetype)initWithView:(id<KHSignupOrLoginViewProtocol>)view {
     if (self = [super init]) {
