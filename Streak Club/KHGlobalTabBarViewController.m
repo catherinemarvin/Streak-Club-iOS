556bf42bdaa0bb0f3bc73d68972fb3a33dbc4dd2
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-27 19:18:39 -0700

    Add browseTabIcon

--- Streak Club/KHGlobalTabBarViewController.m
+++ Streak Club/KHGlobalTabBarViewController.m
@@ -10,16 +10,20 @@
 
 // Tab Bar Sections
 #import "KHHomeViewController.h"
+#import "KHBrowseViewController.h"
 #import "KHSettingsViewController.h"
+
 
 typedef NS_ENUM(NSInteger, KHGlobalTabBarSection) {
     KHGlobalTabBarSectionHome,
+    KHGlobalTabBarSectionBrowse,
     KHGlobalTabBarSectionSettings
 };
 
 @interface KHGlobalTabBarViewController ()
 
 @property (nonatomic, strong) KHHomeViewController *homeViewController;
+@property (nonatomic, strong) KHBrowseViewController *browseViewController;
 @property (nonatomic, strong) KHSettingsViewController *settingsViewController;
 
 @end
@@ -32,11 +36,15 @@ typedef NS_ENUM(NSInteger, KHGlobalTabBarSection) {
         _homeViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Home", nil) image:[UIImage imageNamed:@"homeTabIcon"] tag:KHGlobalTabBarSectionHome];
         UINavigationController *homeNav = [[UINavigationController alloc] initWithRootViewController:_homeViewController];
         
+        _browseViewController = [[KHBrowseViewController alloc] init];
+        _browseViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Browse", nil) image:[UIImage imageNamed:@"browseTabIcon"] tag:KHGlobalTabBarSectionBrowse];
+        UINavigationController *browseNav = [[UINavigationController alloc] initWithRootViewController:_browseViewController];
+        
         _settingsViewController = [[KHSettingsViewController alloc] init];
         _settingsViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Settings", nil) image:[UIImage imageNamed:@"settingsTabIcon"] tag:KHGlobalTabBarSectionSettings];
         UINavigationController *settingsNav = [[UINavigationController alloc] initWithRootViewController:_settingsViewController];
         
-        NSArray *viewControllers = @[homeNav, settingsNav];
+        NSArray *viewControllers = @[homeNav, browseNav, settingsNav];
         [self setViewControllers:viewControllers];
     }
     return self;
