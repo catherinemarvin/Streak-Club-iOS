9c0e2f4760758aa57507d481ba1cbea3ec55223c
Kevin Hwang <k3vinhwang@gmail.com>
2015-05-05 21:32:27 -0700

    Add ability for the viewModel to be configured with a streak

--- Streak Club/KHHomeStreakCellViewModel.h
+++ Streak Club/KHHomeStreakCellViewModel.h
@@ -8,6 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+@class KHStreakModel;
+
+NS_ASSUME_NONNULL_BEGIN
+
 @interface KHHomeStreakCellViewModel : NSObject
 
+@property (nonatomic, strong, readonly) NSString *title;
+@property (nonatomic, strong, readonly) NSString *author;
+@property (nonatomic, strong, readonly) NSString *duration;
+@property (nonatomic, strong, readonly) NSString *participants;
+@property (nonatomic, strong, readonly) NSString *submissions;
+
+- (void)configureWithStreak:(KHStreakModel *)streak;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- Streak Club/KHHomeStreakCellViewModel.m
+++ Streak Club/KHHomeStreakCellViewModel.m
@@ -8,6 +8,28 @@
 
 #import "KHHomeStreakCellViewModel.h"
 
+// Models
+#import "KHStreakModel.h"
+#import "KHUserModel.h"
+
+@interface KHHomeStreakCellViewModel()
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *author;
+@property (nonatomic, strong) NSString *duration;
+@property (nonatomic, strong) NSString *participants;
+@property (nonatomic, strong) NSString *submissions;
+
+@end
+
 @implementation KHHomeStreakCellViewModel
+
+- (void)configureWithStreak:(KHStreakModel *)streak {
+    self.title = @"Streak Title";
+    self.author = streak.host.username;
+    self.duration = [NSString stringWithFormat:NSLocalizedString(@"from %@ to %@", @"Example: from June 15th to June 20th"), streak.startDate, streak.endDate];
+    self.participants = @"5 participants";
+    self.submissions = @"20 submissions";
+}
 
 @end
