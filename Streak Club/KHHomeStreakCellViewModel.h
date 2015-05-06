344c557665f321eadf6aaaabc69595b9c40b26bb
Kevin Hwang <k3vinhwang@gmail.com>
2015-05-05 21:18:20 -0700

    Add a KHHomeStreakCellViewModel to hold the information necessary to configure the cell

---
+++ Streak Club/KHHomeStreakCellViewModel.h
@@ -0,0 +1,13 @@
+//
+//  KHHomeStreakCellViewModel.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 5/5/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHHomeStreakCellViewModel : NSObject
+
+@end
---
+++ Streak Club/KHHomeStreakCellViewModel.m
@@ -0,0 +1,13 @@
+//
+//  KHHomeStreakCellViewModel.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 5/5/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHHomeStreakCellViewModel.h"
+
+@implementation KHHomeStreakCellViewModel
+
+@end
