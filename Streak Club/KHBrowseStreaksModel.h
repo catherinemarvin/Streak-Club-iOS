bea72f2804972923ff199014b6cec78f7fd4a211
Kevin Hwang <k3vinhwang@gmail.com>
2015-05-09 00:16:40 -0700

    Add KHBrowseStreaksModel to hold the result of the browse API call

---
+++ Streak Club/KHBrowseStreaksModel.h
@@ -0,0 +1,13 @@
+//
+//  KHBrowseStreaksModel.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 5/9/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHBrowseStreaksModel : NSObject
+
+@end
---
+++ Streak Club/KHBrowseStreaksModel.m
@@ -0,0 +1,13 @@
+//
+//  KHBrowseStreaksModel.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 5/9/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHBrowseStreaksModel.h"
+
+@implementation KHBrowseStreaksModel
+
+@end
