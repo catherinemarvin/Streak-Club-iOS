2694b4439ff53c6a80356461799d3b5aa4c83f15
Kevin Hwang <k3vinhwang@gmail.com>
2015-06-18 19:14:07 -0700

    Prepare to separate out login/signup

---
+++ Streak Club/KHLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHLoginViewController.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 6/18/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHLoginViewController : UIViewController
+
+@end
---
+++ Streak Club/KHSignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  KHSignupViewController.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 6/18/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHSignupViewController : UIViewController
+
+@end
