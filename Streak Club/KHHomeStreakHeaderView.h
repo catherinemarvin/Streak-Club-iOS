c2b5209851755970c6ff310f6ab5d2559f9961da
Kevin Hwang <k3vinhwang@gmail.com>
2015-06-25 20:53:11 -0700

    Add a header view for the home page

---
+++ Streak Club/KHHomeStreakHeaderView.h
@@ -0,0 +1,19 @@
+//
+//  KHHomeStreakHeaderView.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 6/25/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KHHomeStreakHeaderView : UICollectionReusableView
+
+- (void)setTitle:(NSString *)title;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Streak Club/KHHomeStreakHeaderView.m
@@ -0,0 +1,59 @@
+//
+//  KHHomeStreakHeaderView.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 6/25/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHHomeStreakHeaderView.h"
+
+#import "UIFont+CustomFonts.h"
+#import <Masonry.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KHHomeStreakHeaderView()
+
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@end
+@implementation KHHomeStreakHeaderView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self addSubview:self.titleLabel];
+        [self _initializeAutolayout];
+    }
+    return self;
+}
+
+- (void)setTitle:(NSString * __nonnull)title {
+    NSParameterAssert(title);
+    
+    self.titleLabel.text = title;
+}
+
+#pragma mark - Layout
+
+- (void)_initializeAutolayout {
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.equalTo(self);
+    }];
+}
+
+#pragma mark - Lazy Instantiation
+
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = ({
+            UILabel *label = [[UILabel alloc] init];
+            label.font = [UIFont boldWithSize:24.0f];
+            label;
+        });
+    }
+    return _titleLabel;
+}
+@end
+
+NS_ASSUME_NONNULL_END
