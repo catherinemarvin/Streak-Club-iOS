cab43027c89bf3dec74793a9a9a9b6adab0bc9a9
Kevin Hwang <k3vinhwang@gmail.com>
2015-07-24 20:07:37 -0700

    Add a streak submissions service

---
+++ Streak Club/KHStreakSubmissionsService.h
@@ -0,0 +1,13 @@
+//
+//  KHStreakSubmissionsService.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 7/24/15.
+//  Copyright © 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHStreakSubmissionsService : NSObject
+
+@end
---
+++ Streak Club/KHStreakSubmissionsService.m
@@ -0,0 +1,13 @@
+//
+//  KHStreakSubmissionsService.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 7/24/15.
+//  Copyright © 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHStreakSubmissionsService.h"
+
+@implementation KHStreakSubmissionsService
+
+@end
