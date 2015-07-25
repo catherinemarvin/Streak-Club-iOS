aecf4e98fb1c495bcf46b925306a4f70e54de74d
Kevin Hwang <k3vinhwang@gmail.com>
2015-07-24 19:56:55 -0700

    Add top level streak screen data source

---
+++ Streak Club/KHStreakScreenDataSource.h
@@ -0,0 +1,21 @@
+//
+//  KHStreakScreenDataSource.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 7/24/15.
+//  Copyright © 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ @brief Responsible for coordinating all of the module data sources.
+ */
+
+@interface KHStreakScreenDataSource : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Streak Club/KHStreakScreenDataSource.m
@@ -0,0 +1,13 @@
+//
+//  KHStreakScreenDataSource.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 7/24/15.
+//  Copyright © 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHStreakScreenDataSource.h"
+
+@implementation KHStreakScreenDataSource
+
+@end
