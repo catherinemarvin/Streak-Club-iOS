0a515ea404435207cdc7d828deb3bd9a22299622
Kevin Hwang <k3vinhwang@gmail.com>
2015-08-13 19:47:15 -0700

    Make the module selector view visible

--- Streak Club/KHStreakModuleSelectorView.m
+++ Streak Club/KHStreakModuleSelectorView.m
@@ -8,12 +8,59 @@
 
 #import "KHStreakModuleSelectorView.h"
 
+// ViewModel
+#import "KHStreakModuleSelectorViewModel.h"
+
+// Helper
+#import <Masonry.h>
+#import "UIFont+CustomFonts.h"
+#import "UIColor+HexString.h"
+
 NS_ASSUME_NONNULL_BEGIN
+
+@interface KHStreakModuleSelectorView()
+
+@property (nonatomic, strong) UILabel *selectedModuleLabel;
+
+@end
 
 @implementation KHStreakModuleSelectorView
 
+- (instancetype)init {
+    if (self = [super init]) {
+        [self _configureSubviews];
+        [self _initializeAutolayout];
+    }
+    return self;
+}
+
+- (void)_configureSubviews {
+    [self addSubview:self.selectedModuleLabel];
+}
+
+- (void)_initializeAutolayout {
+    [self.selectedModuleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self);
+    }];
+}
+
 - (void)configureWithViewModel:(nonnull KHStreakModuleSelectorViewModel *)viewModel {
     NSParameterAssert(viewModel);
+    self.selectedModuleLabel.text = viewModel.activeModuleTitle;
+}
+
+#pragma mark - Lazy Instantiation
+
+- (UILabel *)selectedModuleLabel {
+    if (!_selectedModuleLabel) {
+        _selectedModuleLabel = ({
+            UILabel *label = [[UILabel alloc] init];
+            label.font = [UIFont regularWithSize:14.0f];
+            
+            label;
+        });
+    }
+    return _selectedModuleLabel;
 }
 
 @end
