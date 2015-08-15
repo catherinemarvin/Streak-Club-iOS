0d58487252885a6561db54e121605d172fa1622d
Kevin Hwang <k3vinhwang@gmail.com>
2015-08-15 16:23:22 -0700

    Add a recent submissions data source

---
+++ Streak Club/KHRecentSubmissionsDataSource.h
@@ -0,0 +1,13 @@
+//
+//  KHRecentSubmissionsDataSource.h
+//  Streak Club
+//
+//  Created by Kevin Hwang on 8/15/15.
+//  Copyright © 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHRecentSubmissionsDataSource : NSObject
+
+@end
---
+++ Streak Club/KHRecentSubmissionsDataSource.m
@@ -0,0 +1,13 @@
+//
+//  KHRecentSubmissionsDataSource.m
+//  Streak Club
+//
+//  Created by Kevin Hwang on 8/15/15.
+//  Copyright © 2015 Kevin Hwang. All rights reserved.
+//
+
+#import "KHRecentSubmissionsDataSource.h"
+
+@implementation KHRecentSubmissionsDataSource
+
+@end
