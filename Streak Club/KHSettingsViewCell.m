61eacc5f6400981a64ee02126f29a5ea33097d7f
Kevin Hwang <k3vinhwang@gmail.com>
2015-10-12 19:29:28 -0700

    Use proper font for settings view

--- Streak Club/KHSettingsViewCell.m
+++ Streak Club/KHSettingsViewCell.m
@@ -8,16 +8,15 @@
 
 #import "KHSettingsViewCell.h"
 
+#import "UIFont+CustomFonts.h"
+
 @implementation KHSettingsViewCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self.textLabel.font = [UIFont regularWithSize:18.0f];
+    }
+    return self;
 }
 
 @end
