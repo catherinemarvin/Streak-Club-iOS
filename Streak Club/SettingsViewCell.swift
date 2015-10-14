c8ecd06a28a3d1c12babdbcbe677f25bc91779d4
Kevin Hwang <k3vinhwang@gmail.com>
2015-10-13 19:08:10 -0700

    Create settings view cell

---
+++ Streak Club/SettingsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewCell.swift
+//  Streak Club
+//
+//  Created by Kevin Hwang on 10/13/15.
+//  Copyright © 2015 Kevin Hwang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
