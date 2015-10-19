d267690e619cb3d3d02a9a5f2b1a46030c0168ae
Kevin Hwang <k3vinhwang@gmail.com>
2015-10-19 08:30:36 -0700

    Add the rest of the fields in SignupView

--- Streak Club/SignupView.swift
+++ Streak Club/SignupView.swift
@@ -7,15 +7,57 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SignupView: UIView {
-    let formContainer: UIView = {
-        let view = UITextField()
+    private let formContainer: UIView = {
+        let view = UIView()
         view.backgroundColor = UIColor.whiteColor()
         view.layer.cornerRadius = 3
         view.layer.masksToBounds = true
         return view
     }()
+    
+    let usernameField: UITextField = {
+        let field = UITextField()
+        field.placeholder = NSLocalizedString("Username", comment: "Input form for a username")
+        return field;
+    }()
+    
+    let passwordField: UITextField = {
+       let field = UITextField()
+        field.placeholder = NSLocalizedString("Password", comment: "Input form for a password")
+        field.secureTextEntry = true
+        return field
+    }()
+    
+    let repeatPasswordField: UITextField = {
+        let field = UITextField()
+        field.placeholder = NSLocalizedString("Repeat password", comment: "Input form to confirm password")
+        field.secureTextEntry = true
+        return field;
+    }()
+    
+    let emailField: UITextField = {
+        let field = UITextField()
+        field.placeholder = NSLocalizedString("Email", comment: "Input form for a user's email")
+        return field
+    }()
+    
+    let actionField: UIButton = {
+        let button = UIButton()
+        button.setTitle(NSLocalizedString("Create Account", comment: "Button for creating an account"), forState: UIControlState.Normal)
+        button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        return button
+    }()
+    
+    let switchModeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(NSLocalizedString("Already have an account?", comment: "Button for switching between registering and logging in."), forState: UIControlState.Normal)
+        return button
+    }()
+    
+    
     
     init() {
         super.init(frame: CGRectZero)
