d4d55e04e7d3fb3a516fbe97425b489412ec69fe
Kevin Hwang <k3vinhwang@gmail.com>
2015-10-26 23:44:13 -0700

    Add streak progress bar

--- Streak Club/StreakProgressBar.swift
+++ Streak Club/StreakProgressBar.swift
@@ -7,22 +7,46 @@
 //
 
 import UIKit
+import SnapKit
 
 final class StreakProgressBar : UIView {
+    private let filledInView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hexString: "aada80")
+        
+        return view
+    }()
+    
+    var completionPercentage: Int = 0 {
+        didSet {
+            self.setNeedsUpdateConstraints()
+        }
+    }
+    
     init() {
         super.init(frame: CGRectZero)
-        self.backgroundColor = UIColor.blackColor()
+        self.backgroundColor = UIColor(hexString: "dadada")
+        _initializeAutolayout()
+    }
+    
+    private func _initializeAutolayout() {
+        self.snp_makeConstraints { (make) -> Void in
+            make.height.equalTo(20)
+        }
+        
+        self.filledInView.snp_makeConstraints { (make) -> Void in
+            make.left.top.bottom.equalTo(self)
+        }
+    }
+    
+    override func updateConstraints() {
+        filledInView.snp_updateConstraints { (make) -> Void in
+            make.width.equalTo(self).multipliedBy(completionPercentage)
+        }
+        super.updateConstraints()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    lazy var filledInView: UIView = {
-        let view = UIView.init()
-        view.backgroundColor = UIColor.redColor()
-        
-        return view
-    }()
-    
 }
